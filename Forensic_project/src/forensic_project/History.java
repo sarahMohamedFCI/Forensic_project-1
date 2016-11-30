e6706017c850c6771689a8d1d10403d2328e7c92
sarahMohamedFCI <sarahm.salama@outlook.com>
2016-11-30 14:58:45 +0200

    Update History.java
    testing

--- Forensic_project/src/forensic_project/History.java
+++ Forensic_project/src/forensic_project/History.java
@@ -13,16 +13,22 @@ import forensic_project.Tokens;
  * @author Ayaa
  */
 public class History implements IFile {
+    
     //array list of history content
     public ArrayList<HistoryContent> historyContent=new ArrayList<HistoryContent>();
+    
     //list of titles
     public ArrayList<String> TitlesList=new ArrayList<String>();
+    
     //list of urls
     public ArrayList<String> URLList=new ArrayList<String>();
+    
     //list of pagecontent words
      public ArrayList<String> PageContentList=new ArrayList<String>();
-     //intersts list
-     public ArrayList<String> InterestsList=new ArrayList<String>();
+    
+    //intersts list
+    public ArrayList<String> InterestsList=new ArrayList<String>();
+    
     //file path
     public String path;
     
@@ -36,27 +42,32 @@ public class History implements IFile {
     public void SetFilePath(String path) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
     //get file of history,cashes,...
     @Override
     public String GetFilePath() {
         
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
     //get keywords from title
     public ArrayList<String> GetTitleKeyword(ArrayList<String> TitleList){
         
        return TitleList; 
     }
+    
     //get url keywords
     public ArrayList<String> GetURLKeyword(ArrayList<String> URLList){
         
        return URLList; 
     }
+    
     //get page content keywords
     public ArrayList<String> GetPageContentKeyword(ArrayList<String> PageContentList){
         
        return PageContentList; 
     }
+    
     //find interests from keywords list
     public ArrayList<String> FindInterest(ArrayList<String> TitleList,ArrayList<String> URLList,ArrayList<String> PAgeContentList){
        
